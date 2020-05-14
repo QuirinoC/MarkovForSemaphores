01f3fcb3617fd326c816926b049ae8ceb72d768a
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-05-13 23:49:23 -0600

    haha car goes vrrrroom

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from car import Car
 from map import Map
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+
+async def main():
+  for i in range(5):
+    x = asyncio.create_task(Car().vroom({}))
+
+  await x
+
+asyncio.run(main())
