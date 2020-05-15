cefb3b38907d5055fb91903baa12b5eed29fe420
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-05-15 13:52:11 -0600

    Refactored code'

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ from graph import Graph
 from random import randint, random
 import asyncio
 
+# Markov graph path
+GRAPH_PATH = 'graph.txt'
+MAP_PATH   = 'mapa_symbols.txt'
+
+
 async def block():
   # Block for ever
   while True:
     await asyncio.sleep(0xFFFFFFFFFFFF)
 
 async def main():
-  car_map = {}
-  map = Map('mapa_symbols.txt', car_map)
-  car_map[0] = Car(15, 10, graph)
-  car_map[1] = Car(15, 11, graph)
+  zapopan_graph = Graph(GRAPH_PATH)
+  zapopan_map   = Map(MAP_PATH)
 
-  for i in car_map:
-    asyncio.create_task(car_map[i].vroom())
-  asyncio.create_task(map.render_map())
+  await zapopan_map.render_map()
 
-
-  await block()
 
 if __name__ == "__main__":
   asyncio.run(main())
