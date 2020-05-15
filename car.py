93b57355c2e76b5083caa473a0e39c7abcf631c0
Carlos Cabello <carloscabello364@gmail.com>
2020-05-14 21:14:35 -0500

    Street testing

--- car.py
+++ car.py
@@ -4,22 +4,28 @@ from random import random, randint
 
 car_colors = ['🚖', '🚗', '🚘', '🚙', '🚍']
 dir_options = ['up', 'down', 'left', 'right']
+street_options = [0, 1, 2, 3] # A, B, C, D
 
 class Car:
   def __init__(self,
                x,
                y,
+               graph,
                speed=1,
                color=None,
                dir=None,
+               street=None,
               ):
 
-    self.x     = x
-    self.y     = y
-    self.speed = randint(1,5) if speed is None else speed
-    self.color = choice(car_colors) if color is None else car_colors[color]
-    self.dir   = choice(dir_options) if dir is None else dir
-    self.run   = True
+    self.x      = x
+    self.y      = y
+    self.graph  = graph
+    self.speed  = randint(1,5) if speed is None else speed
+    self.color  = choice(car_colors) if color is None else car_colors[color]
+    self.dir    = choice(dir_options) if dir is None else dir
+    self.street = choice(street_options) if street is None else street
+    self.run    = True
+    self.f = open("log.txt", "w")
 
   def __str__(self):
     return self.color
@@ -34,9 +40,20 @@ class Car:
       'left' : (-1,0),
       'right' : (1 ,0),
     }
+    self.decide()
     x, y = dir_map[self.dir]
     self.x += x
     self.y += y
+
+  def decide(self):
+    self.f.writelines("Street: " + self.street + "\n")
+    graphRow = self.graph[self.street]
+    accum = 0
+    limit = random()
+    for i, p in enumerate(graphRow):
+      accum += p
+      if limit < accum:
+        self.dir = dir_options[i]
 
   async def vroom(self):
     while self.run:
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from car import Car
 from map import Map
+from graph import Graph
 from random import randint
 import asyncio
 
@@ -10,8 +11,15 @@ async def block():
 
 async def main():
   car_map = {}
-  map = Map('mapa_4.txt', car_map)
-  car_map[0] = Car(15,0)
+  map = Map('mapa_4_X.txt', car_map)
+  graph = [
+            # L    R    F    U
+            [0.2, 0.49, 0.3, 0.01], #A
+            [0.25, 0.15, 0.5, 0.1], #B
+            [0.4, 0.3, 0.3, 0.0],   #C
+            [0.25, 0.15, 0.5, 0.1]  #D
+        ]
+  car_map[0] = Car(15, 10, graph)
 
   asyncio.create_task(car_map[0].vroom())
   asyncio.create_task(map.render_map())
